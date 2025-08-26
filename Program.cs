6fe00594ec500168d3885cb68ea55e657a70d50a
ajfm88 <ale@ajfm88.com>
2025-08-26 09:17:55 -0700

    01. Programming Basics. 02. Variables.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int age = 25, health = 100, power = 5;
+
+Console.WriteLine(age);
+
+age = 27;
+
+age++;
+
+age += 1;
+
+age = age + 1;
+
+Console.WriteLine(age);
