3169c184c406a6aa418e521625f0e328e5d7139c
ajfm88 <ale@ajfm88.com>
2025-09-13 21:33:07 -0700

    04. If Statements, Conditions.

--- Program.cs
+++ Program.cs
@@ -2,30 +2,10 @@
 Console.WriteLine("Hello, World!");
 
 int age = 35;
-double age2 = 25.5;
-float position = 35.5f;
-Console.WriteLine("Number" + 5 + 6);
-Console.WriteLine(5 + 6 + "Number");
-
-string name = "Alejandro";
-
-Console.WriteLine("My name is " + name + " and I'm " + age + " years old.");
-Console.WriteLine($"My name is {name} and I'm {age} years old.");
-
-char character = 'a';
-Console.WriteLine("My name is \"Alejandro Foucault\"");
-
-bool isPlayer = true;
-bool isEnemy = false;
-
-var myVariable = 10.2;
-
-//long ageUniverse = 15;
-//int ageUniverseInt = ageUniverse;
-
-int ageUniverseInt = 15;
-long ageUniverse = ageUniverseInt;
-
-int health = Convert.ToInt32("56");
-int healthMax = 10;
-Console.WriteLine((float)health / healthMax);
+if (age < 20) {
+    Console.WriteLine("Age under 20!");
+} else if (age < 30) {
+    Console.WriteLine("Age above 20 and under 30!");
+} else {
+    Console.WriteLine("Age above 30")
+}
