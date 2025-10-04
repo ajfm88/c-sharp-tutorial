135323a95aea57afe3afb6e65592921fbc77ea74
ajfm88 <ale@ajfm88.com>
2025-10-04 10:24:46 -0700

    09. Arrays, Lists.

--- Program.cs
+++ Program.cs
@@ -4,19 +4,22 @@
     {
         static void Main(string[] args)
         {
-            int age = 35;
+            int[] numbersArray = new int[] { 56, 123, 12 };
+            int[] numbersArray2 = new int[5];
+            int[] numbersArray3 = new int[7] { 13, 43, 78, 29, 56, 123, 12 };
+            Console.WriteLine(numbersArray.Length);
+            Console.WriteLine(numbersArray[0]);
+            Console.WriteLine(System.Array.IndexOf(numbersArray, 123));
 
-            SayHello("Alejandro", 23);
-
-            // my comment
-            /* my 
-            multiline
-            comment*/
-        }
-
-        static void SayHello(string playerName, int age)
-        {
-            Console.WriteLine($"Hello {playerName}, you are {age} years old!");
+            List<int> numbersList = new List<int>();
+            numbersList.Add(12);
+            numbersList.Add(0);
+            numbersList.Add(89);
+            Console.WriteLine(numbersList.Count);
+            Console.WriteLine(numbersList[0]);
+            Console.WriteLine(numbersList.Contains(89));
+            Console.WriteLine(numbersList.Contains(37));
+            Console.WriteLine(numbersList.IndexOf(37));
         }
     }
 }
