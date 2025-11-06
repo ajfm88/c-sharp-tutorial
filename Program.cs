711207cffa8f4ea775b6fc482fad2253a31fb2fc
ajfm88 <ale@ajfm88.com>
2025-11-06 10:53:44 -0800

    13. Access Modifiers.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
 ﻿namespace c_sharp_tutorial
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Player codeMonkeyPlayer = new Player("Hideo Kojima");
-            Player ironManPlayer = new Player("Tony Stark");
             codeMonkeyPlayer.SayHello();
+            Player ironManPlayer = new Player("Tony Stark");
+            ironManPlayer.SayHello();
 
             Console.WriteLine("");
         }
 
-        class Player
+        private class Player
         {
-            public static string name = "-";
-            public static void SayHello()
+            private string name = "-";
+
+            public Player (string name)
+            {
+                this.name = name;
+                Console.WriteLine($"Creating player {name}!");
+
+                SayHello();
+            }
+            public void SayHello()
             {
                 Console.WriteLine($"Hello {name}!");
             }
