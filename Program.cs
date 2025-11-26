fb09d5fbc43f342d8b8f401fe5e6531d6d5d5227
ajfm88 <ale@ajfm88.com>
2025-11-26 11:19:38 -0800

    17. Multidimensional Arrays.

--- Program.cs
+++ Program.cs
@@ -1,31 +1,39 @@
 ﻿using System.Text;
-using CodeMonkey_CSharpCourse;
 
 namespace CodeMonkey_CSharpCourse
 {
     public class Program
     {
-        public static string PlayerName {get; set;}
-        private static string playerName;
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+    
+            int[][] intArray = new int[5][];
+            intArray[0] = new int[3];
+            intArray[1] = new int[6];
+            intArray[2] = new int[10];
 
-            PlayerName = "Code Monkey";
-            Console.WriteLine(PlayerName);
+            for (int i=0; i < intArray.GetLength(0); i++)
+            {
+                Console.WriteLine(intArray[i] == null);
+                intArray[i] = new int[6];
+            }
+
+            intArray[0][2] = 5;
+
+            /*
+            int[,] intArray = new int[,] { { 1, 2, 3}, { 4, 5, 6} };
+
+            intArray[0, 2] = 2;
+
+            for (int i=0; i < intArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < intArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(intArray[i, j]);
+                }
+            }
+            */
         }
-
-            private static void SetPlayerName(string playerName)
-        {
-            Program.playerName = playerName;
-        }
-    }
-}
-
-public class Player
-{
-    public Player()
-    {
-        Console.WriteLine(Program.PlayerName);
     }
 }
