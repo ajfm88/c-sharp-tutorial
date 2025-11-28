7c13508d73b8db2b629056940441c5914aa419de
ajfm88 <ale@ajfm88.com>
2025-11-28 10:20:55 -0800

    18. Nested Loops.

--- Program.cs
+++ Program.cs
@@ -7,33 +7,16 @@ namespace CodeMonkey_CSharpCourse
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-    
-            int[][] intArray = new int[5][];
-            intArray[0] = new int[3];
-            intArray[1] = new int[6];
-            intArray[2] = new int[10];
 
-            for (int i=0; i < intArray.GetLength(0); i++)
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(intArray[i] == null);
-                intArray[i] = new int[6];
-            }
-
-            intArray[0][2] = 5;
-
-            /*
-            int[,] intArray = new int[,] { { 1, 2, 3}, { 4, 5, 6} };
-
-            intArray[0, 2] = 2;
-
-            for (int i=0; i < intArray.GetLength(0); i++)
-            {
-                for (int j = 0; j < intArray.GetLength(1); j++)
+                for (int j = 0; j < 2; j++)
                 {
-                    Console.WriteLine(intArray[i, j]);
+                    Console.WriteLine(i + ", " + j);
+                    // break;
+                    // continue;
                 }
             }
-            */
         }
     }
 }
