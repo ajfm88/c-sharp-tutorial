ed88aad1cade7ffdd52ccfb38ece39b58f4bdb99
ajfm88 <ale@ajfm88.com>
2025-12-04 11:53:30 -0800

    20. Dictionary.

--- Program.cs
+++ Program.cs
@@ -4,28 +4,51 @@ namespace CodeMonkey_CSharpCourse
 {
     public class Program
     {
+        private enum ResourceType
+        {
+            Stone,
+            Wood,
+            Gold,
+        }
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            int number = 0;
-            while (number < 5)
-            {
-                number++;
-            }
-            Console.WriteLine(number);
+            // Creates a new dictionary
+            // Dictionary<ResourceType, int> resourceTypeAmountDictionary = new Dictionary<ResourceType, int>();
 
-            Console.WriteLine(RecursionIncrementer(0, 5, 1));
-        }
+            // Another way to create key-value pairs and add them to the dictionary
+            Dictionary<ResourceType, int> resourceTypeAmountDictionary = new Dictionary<ResourceType, int>()
+            {
+                {ResourceType.Wood, 10},
+                {ResourceType.Gold, 27},
 
-        private static int RecursionIncrementer(int number, int numberMax, int incrementAmount)
-        {   
-            if (number < 5)
+            };
+
+            // Creates key-value pair and adds it to the dictionary
+            resourceTypeAmountDictionary.Add(ResourceType.Stone, 56);
+
+            Console.WriteLine(resourceTypeAmountDictionary[ResourceType.Stone]);
+
+            // Update value property in key-value pair
+            resourceTypeAmountDictionary[ResourceType.Stone] = 45;
+
+            // Check if a value exists
+            resourceTypeAmountDictionary.ContainsKey(ResourceType.Wood);
+
+            resourceTypeAmountDictionary[ResourceType.Wood] = 30;
+            // Another way to check if a value exists
+            if (resourceTypeAmountDictionary.TryGetValue(ResourceType.Wood, out int woodAmount))
             {
-                return RecursionIncrementer(number + incrementAmount, numberMax, incrementAmount);
-            } else
+                Console.WriteLine(woodAmount);
+            };
+
+            // remove a poperty from a dictionary
+            resourceTypeAmountDictionary.Remove(ResourceType.Wood);
+
+            foreach (ResourceType resourceType in resourceTypeAmountDictionary.Keys)
             {
-                return number;
+                Console.WriteLine(resourceType + ": " + resourceTypeAmountDictionary[resourceType]);
             }
         }
     }
