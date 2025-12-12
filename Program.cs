8806b9e51769a6dc972b3821b6508700626214cb
ajfm88 <ale@ajfm88.com>
2025-12-11 16:01:45 -0800

    23. Optional Parameters.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,33 @@
-﻿using System.Text;
+﻿using System.Numerics;
 
 namespace CodeMonkey_CSharpCourse
 {
     public class Program
     {
+        private const int DEFAULT_HEALTH_AMOUNT = 100;
+        private enum UnitType
+        {
+            Melee,
+            Ranged,
+            Wizard
+        }
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            PrintPlayerName("Alejandro Foucault", "Lionel Messi");
+            SpawnUnit(new Vector3(0, 0, 0), UnitType.Melee, Quaternion.Identity, 56);
+            SpawnUnit(new Vector3(10, 5, 0), UnitType.Ranged, Quaternion.Identity);
+            SpawnUnit(healthAmount: 56);
         }
-        // params is the keyword that we use when we want to add any number of parameters into a function.
-        private static void PrintPlayerName(params string[] playerNameArray)
+        private static void SpawnUnit(
+            Vector3 spawnPosition = default,
+            UnitType unitType = UnitType.Melee,
+            Quaternion spawnRotation = default,
+            int healthAmount = DEFAULT_HEALTH_AMOUNT
+            )
         {
-            Console.WriteLine(playerNameArray.Length);
+            Console.WriteLine("Spawning unit of type " + unitType + " with health: " + healthAmount + " at " + spawnPosition + ", ");
         }
     }
 }
